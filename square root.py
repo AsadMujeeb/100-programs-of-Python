199573374596eb13db42931214a540719fc36530
Asad Mujeeb <128974395+AsadMujeeb@users.noreply.github.com>
2023-11-22 23:58:23 +0500

    Update square root.py

--- square root.py
+++ square root.py
@@ -14,3 +14,12 @@ number = int(input("Enter the number :  "))
 sqrt = number ** (1 / 2)
 
 print("The square root of number is :", sqrt)
+
+# uaing math module
+import math
+
+num = int(input("Enter the number :"))
+sr = math.sqrt(num)
+
+print("The square root of number is: ", sr)
+
