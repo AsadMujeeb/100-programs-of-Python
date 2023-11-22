78f46495e860e84dea7ce98aa82e343c19119d72
Asad Mujeeb <128974395+AsadMujeeb@users.noreply.github.com>
2023-11-23 00:19:52 +0500

    Add files via upload

---
+++ is positive or negative.py
@@ -0,0 +1,12 @@
+
+
+number = int(input("Enter the number : "))
+
+if number > 0 :
+    print("Number is positive")
+
+elif number == 0:
+    print("It is zero") 
+    
+else:
+    print("It is negative number")
