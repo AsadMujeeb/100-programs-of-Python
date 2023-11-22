f5f178fe8da6c70ddeef79fdfcc79f15ad9acc4c
Asad Mujeeb <128974395+AsadMujeeb@users.noreply.github.com>
2023-11-22 23:55:43 +0500

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,16 @@
+# using exponention
+
+number = 64
+
+sqrt = number ** (1 / 2)
+
+print("The square root of number is : ", sqrt)
+
+
+# taking input from user
+
+number = int(input("Enter the number :  "))
+
+sqrt = number ** (1 / 2)
+
+print("The square root of number is :", sqrt)
