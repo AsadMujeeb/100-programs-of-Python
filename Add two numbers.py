0c1ce9ebfbf1f53ced74b5e1bb8d4391d2d2bb77
Asad Mujeeb <128974395+AsadMujeeb@users.noreply.github.com>
2023-11-22 23:40:44 +0500

    Add files via upload

---
+++ Add two numbers.py
@@ -0,0 +1,18 @@
+# pre-defined variables
+
+num1 = 12 
+
+num2 = 34 
+
+sum = num1 + num2
+
+print("The sum of two numbers : ", sum)
+
+# user input
+
+num1 = float(input("Enter the number : "))
+num2 = flaot(input("Enter the number : "))
+
+sum = num1 + num2 
+
+print("The sum of two numbers : ", sum)
