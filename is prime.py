2dfaaab5169d84664f3fe62984f4fd32a8423545
Asad Mujeeb <128974395+AsadMujeeb@users.noreply.github.com>
2023-11-23 18:20:14 +0500

    Add files via upload

---
+++ is prime.py
@@ -0,0 +1,22 @@
+num = int(input("Enter the number : "))
+
+if num == 1 :
+    
+    print("It is not a prime number ")
+
+if num > 1 :
+    
+    for i in range(2 , num):
+        
+        if num % i == 0:
+            
+            print("Not Prime ")
+            
+            break
+        
+else:
+    
+            print("Prime Number")
+
+
+
